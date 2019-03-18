7968dcb46595bf74db39b982fb8f742f329c56c8
ZxxWs <zxx748361145@outlook.com>
2019-03-18 20:34:26 +0800

    蓝桥

--- 蓝桥/作业.cpp
+++ 蓝桥/作业.cpp
@@ -1,116 +1,161 @@
-#include<iostream>
-using namespace std;
-
-
-//分治法 循环赛问题
-
-
-
-//减治法 可变规模问题，求数列n中第k小的数
-int arr[1000];
-int kk(int x,int y) {
-	int j,m;
-	m = arr[x];
-	j = x;
-	while (x<=y)
-	{
-		while (m<arr[y])
-		{
-			y--;
-		}
-		if(m>arr[y]) {
-			arr[j] = arr[y];
-			j = y;
-			y--;
-			}
-		while (m>arr[x])
-		{
-			x++;
-		}
-		if (m < arr[x]) {
-			arr[j] = arr[x];
-			j = x;
-			x++;
-		}	
-	}
-	arr[j] = m;
-	return j;
-}
-
-
-int main() {
-	int n,  k;
-	cout << "输入数字个数" << endl;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-	cout<<"输入查找的第几小的元素"<<endl;
-	cin >> k;
-
-	int lj = 0, rj = n - 1;
-	int j = kk(lj, rj);
-	cout << j << "j是这个" << endl;
-	while(n)
-	{
-
-		if ((j+1) == k) {
-			cout << "第" << k  << "小的数是" << arr[j] << endl;
-			return 0;
-		}
-		if (j > k) {
-			rj = j-1;
-			j = kk(lj,j);
-			
-		}
-		if (j < k) {
-			lj = j-1;
-			j = kk(j,rj);
-			
-		}
-
-		n--;
-	}
-
-}
-
-
-//typedef struct point {
-//	int x = 0x3f3f3f3f;
-//	int y = 0x3f3f3f3f;
-//}point;
+//#include<iostream>
+//using namespace std;
 //
-//int main(){//非书本方法
-//	int n;
-//	cin >> n;
-//	cin >> n;
-//	point a[100];
-//	for (int i = 0; i < n; i++) {
-//		cin >> a[i].x >> a[i].y;
-//	}
-//	int mix = 0x3f3f3f3f;
-//	int pointn= 0, pointm = 0;
-//	int distance[100][100] = { 0 };
-//	for (int i = 0; i < n-1; i++) {
-//		for (int j = i + 1; j < n; j++)
+//
+////分治法 循环赛问题
+//
+//
+//
+////减治法 可变规模问题，求数列n中第k小的数
+//int arr[1000];
+//int kk(int x,int y) {
+//	int j,m;
+//	m = arr[x];
+//	j = x;
+//	while (x<=y)
+//	{
+//		while (m<arr[y])
 //		{
-//			distance[i][j] = ((a[i].x - a[j].x)*(a[i].x - a[j].x)) + ((a[i].y - a[j].y)*(a[i].y - a[j].y));
-//			if (distance[i][j] < mix&&distance[i][j] != 0) {
-//				
-//				mix = distance[i][j];
-//					pointn = i+1;
-//				pointm = j + 1;
-//			}
+//			y--; 
+//			if (x > y)
+//				break;
 //		}
+//		if(m>arr[y]) {
+//			arr[j] = arr[y];
+//			j = y;
+//			y--;
+//			}
+//		while (m>arr[x])
+//		{
+//			x++;
+//			if (x = y)break;
+//		}
+//		if (m < arr[x]) {
+//			arr[j] = arr[x];
+//			j = x;
+//			x++;
+//		}	
 //	}
-//	cout << pointn << '\0' << pointm << endl;
-//	return 0;
+//	arr[j] = m;
+//
+//	return j;
+//}
+//int main() {
+//	int n,  k;
+//	cout << "输入数字个数" << endl;
+//	cin >> n;
+//	cout << "请依次输入数据" << endl;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	cout<<"输入查找的第几小的元素"<<endl;
+//	cin >> k;
+//	int lj = 0, rj = n - 1;
+//	int j = kk(lj, rj);
+//	cout << j << endl;
+//	while(n>0)
+//	{
+//		cout << j << endl;
+//		
+//		if ((j+1) == k) {
+//			cout << "第" << k  << "小的数是" << arr[j] << endl;
+//			return 0;
+//		}
+//		if (j+1 > k) {
+//			rj = j-1;
+//			j = kk(lj,j);
+//		}
+//		if (j+1 < k) {
+//			lj = j-1;
+//			j = kk(j,rj);
+//		}
+//		n--;
+//	}
+//
 //}
 //
 //
-//
-//
+////
+////#include <iostream>
+////#include <iomanip>
+////using namespace std;
+////#define Max 100
+////
+////int Search(int a[], int n, int k);
+////void Print(int a[], int n);
+////
+////int main()
+////{
+////	int a[] = { 98,33,21,102,45,5,32,11,65,82,193,321,34,72 };
+////	int n, k, x;
+////	n = sizeof(a) / sizeof(a[0]);
+////	cout << "请输入要查找的第几小的元素值:" << endl;
+////	cin >> k;
+////	x = Search(a, n, k);
+////	cout << "数组中的元素:" << endl;
+////	Print(a, n);
+////	cout << "第" << k << "小的元素值是:";
+////	cout << x << endl;
+////	return 0;
+////}
+////
+////int Search(int a[], int n, int k)//快速排序寻找数组中第k小的元素 
+////{
+////	int low, high, i, j, t;
+////	k--;
+////	low = 0;
+////	high = n - 1;
+////	do
+////	{
+////		i = low;
+////		j = high;
+////		t = a[low];//将a[low]作为枢轴元素 
+////		do
+////		{
+////			while (i < j&&t < a[j])//若当前元素大于枢轴元素t，则将j向左移动比较下一元素 
+////			{
+////				j--;
+////			}
+////			if (i < j)//若当前元素小于或等于t,则将当前元素存入a[j]; 
+////			{
+////				a[i++] = a[j];
+////			}
+////			while (i < j&&t >= a[i])//若当前元素小于或等于枢轴元素t,则将i向右移动比较下一元素 
+////			{
+////				i++;
+////			}
+////			if (i < j)//若当前元素大于枢轴元素,则将元素存入a[j]; 
+////			{
+////				a[j--] = a[i];
+////			}
+////		} while (i < j);
+////		a[i] = t;//枢轴元素存入a[i]; 
+////		if (i == k)//若找到第k小元素 结束循环 
+////		{
+////			break;
+////		}
+////		if (i < k)//说明查找的元素在右半部分 
+////		{
+////			low = i + 1;
+////		}
+////		else//说明查找的元素在左半部分 
+////		{
+////			high = i - 1;
+////		}
+////	} while (i != k);
+////	return a[k];//返回第k小元素的值 
+////}
+////
+////void Print(int a[], int n)
+////{
+////	int i;
+////	for (i = 0; i < n; i++)
+////	{
+////		cout << setw(4) << a[i];
+////	}
+////	cout << endl;
+////}
 //
 //
 //
--- 蓝桥/最大子段和.cpp
+++ 蓝桥/最大子段和.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
-
-using namespace std;
-
-
-
+//#include<iostream>
+//
+//using namespace std;
+//
+//
+//int main() {
+//
+//	int arr[1000];
+//	int n;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cin >> arr[i];
+//	}
+//	int num = arr[0];
+//	for (int i = 1; i < n; i++) {
+//		
+//		if (num+arr[i] >= arr[i]) {
+//			num += arr[i];
+//			continue;
+//		}
+//		if (num+arr[i] < arr[i]) {
+//			num = arr[i];
+//			continue;
+//		}
+//	}
+//	cout << num;
+//	return 0;
+//}
