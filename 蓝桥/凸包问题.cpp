37921009cb800bf72ab85cdb8e2fee53a79d1ca1
ZxxWs <zxx748361145@outlook.com>
2019-04-20 20:38:04 +0800

    日更

---
+++ 蓝桥/凸包问题.cpp
@@ -0,0 +1,126 @@
+﻿//#include<iostream>
+//using namespace std;
+//typedef struct Point
+//{
+//	float x, y;
+//	int flag;
+//}Point;
+//Point point[100];
+//int n;
+//
+//void ConvexHull() {
+//	for (int i = 0; i < n; ++i) {
+//		for (int j = i + 1; j < n; ++j) {
+//			float a = point[j].y - point[i].y;
+//			float b = point[j].x - point[i].x;
+//			float c = (point[i].x*point[j].y) - (point[i].y*point[j].x);
+//
+//			int sign1 = 0;
+//			int sign2 = 0;
+//
+//			for (int k = 0; k < n; k++) {
+//				if ((k == j) || (k == i))continue;
+//				if ((a*point[k].x + b * point[k].y) == c) {
+//					++sign1;
+//					++sign2;
+//				}
+//				if ((a*point[k].x + b * point[k].y) > c) {
+//					++sign1;
+//				}
+//				if ((a*point[k].x + b * point[k].y) < c) {
+//					++sign2;
+//				}
+//			}
+//			if (((sign1 == (n - 2)) || (sign2 == (n - 2)))){
+//				point[i].flag = 1;
+//				point[j].flag = 1;
+//			}
+//		}
+//	}
+//
+//}
+//
+//int main() {
+//
+//	cout << "输入点的个数" << endl;
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cout << "输入第" << i + 1 << "个点的x  y坐标：";
+//		cin >> point[i].x >> point[i].y;
+//	}
+//	ConvexHull();
+//	cout << "边界点分别为：" << endl;
+//	for (int i = 0; i < n; i++) {
+//		if (point[i].flag == 1)
+//			cout << "x:" << point[i].x << "   y:" << point[i].y << endl;
+//	}
+//}
+//
+//
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
+
+typedef struct p {
+	int x, y, flag;
+}Point;
+Point point[100];
+int n;
+
+void con() {
+	int a, b, c;
+	int sign1, sign2;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			a = point[j].y - point[i].y;
+			b = point[i].x - point[j].x;
+			c = (point[i].x*point[j].y) - (point[i].y*point[j].x);
+			sign1 = 0;
+			sign2 = 0;
+			for (int k = 0; k < n; k++) {
+				if ((k == j) || (k == i))continue;
+				if ((a*point[k].x + b * point[k].y) == c) {
+					sign1++;
+					sign2++;
+				}
+				if ((a*point[k].x + b * point[k].y) > c)
+				{
+					sign1++;
+				}
+				if ((a*point[k].x + b * point[k].y) < c)
+				{
+					sign2++;
+				}
+
+			}
+
+			if ((sign1 == n-2) || (sign2 == n-2)) {
+				point[i].flag = 1;
+				point[j].flag = 1;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (point[i].flag == 1) {
+			cout << point[i].x << "  " << point[i].y << endl;
+		}
+	}
+}
+int  main() {
+
+	for (int i = 0; i < n; i++) {
+		point[i].flag = 0;
+		cin>>point[i].x;
+		cin>>point[i].y;
+	}
+	cout << "坐标点:" << endl;
+
+	cout << "极坐标点:" << endl;
+	con();
+	for (int i = 0; i < n; i++) {
+		if(point[i].flag==1)
+		cout << point[i].x << "  " << point[i].y << endl;
+	}
+	return 0;
+}
--- 蓝桥/活动安排.cpp
+++ 蓝桥/活动安排.cpp
@@ -1,51 +1,82 @@
-//#include<iostream>
-//
+////#include<iostream>
+////
+////using namespace std;
+////
+////int n;
+////int s[100] = { 0 };
+////int f[100] = { 0 };
+////bool a[100] = { 0 };
+////int ActiveManage() {
+////
+////	a[1] = 1;
+////	int j = 1, cout = 1;
+////	for (int i = 2; i <= n; i++) {
+////		if (s[i] >= f[j]) {
+////			a[i] = 1;
+////			j = i;
+////			cout++;
+////		}
+////		else
+////			a[i] = 0;
+////	}
+////	return cout;
+////
+////}
+////
+////int main()//
+////{
+////	cin >> n;
+////	for (int i = 0; i < n; i++) {
+////		cin >> s[i] >> f[i];
+////	}
+////	
+////	cout<<ActiveManage();
+////}
+//#include <iostream>
+//#include <string>
 //using namespace std;
 //
-//int n;
-//int s[100] = { 0 };
-//int f[100] = { 0 };
-//bool a[100] = { 0 };
-//int ActiveManage() {
-//
-//	a[1] = 1;
-//	int j = 1, cout = 1;
-//	for (int i = 2; i <= n; i++) {
-//		if (s[i] >= f[j]) {
-//			a[i] = 1;
-//			j = i;
-//			cout++;
-//		}
-//		else
-//			a[i] = 0;
-//	}
-//	return cout;
-//
-//}
-//
-//int main()//
-//{
-//	cin >> n;
-//	for (int i = 0; i < n; i++) {
-//		cin >> s[i] >> f[i];
-//	}
-//	
-//	cout<<ActiveManage();
-//}
-#include <iostream>
-#include <string>
-using namespace std;
-
+////int main()
+////{
+////	int j;
+////	int n;
+////	//string S;
+////	//string T;
+////	char S[100],T[100];
+////	cin >> n;
+////	for (int i = 0; i < n; i++)
+////		cin >> S[i];
+////	int x = 0, y = n - 1;
+////	for (j = 0; j < n; j++)
+////	{
+////		if (S[x] >= S[y])
+////		{
+////			T[j] = S[y];
+////			y--;
+////			continue;
+////		}
+////		else
+////		{
+////			T[j] = S[x];
+////			x++;
+////			continue;
+////		}
+////	}
+////	for (int i = 0; i < n; i++)
+////		cout << T[i];
+////
+////
+////	return 0;
+////}
 //int main()
 //{
 //	int j;
 //	int n;
-//	//string S;
-//	//string T;
-//	char S[100],T[100];
 //	cin >> n;
-//	for (int i = 0; i < n; i++)
-//		cin >> S[i];
+//	string S;
+//	string T;
+//	cin >> S;
+//	T = S;
 //	int x = 0, y = n - 1;
 //	for (j = 0; j < n; j++)
 //	{
@@ -62,38 +93,7 @@ using namespace std;
 //			continue;
 //		}
 //	}
-//	for (int i = 0; i < n; i++)
-//		cout << T[i];
-//
+//	cout << T;
 //
 //	return 0;
 //}
-int main()
-{
-	int j;
-	int n;
-	cin >> n;
-	string S;
-	string T;
-	cin >> S;
-	T = S;
-	int x = 0, y = n - 1;
-	for (j = 0; j < n; j++)
-	{
-		if (S[x] >= S[y])
-		{
-			T[j] = S[y];
-			y--;
-			continue;
-		}
-		else
-		{
-			T[j] = S[x];
-			x++;
-			continue;
-		}
-	}
-	cout << T;
-
-	return 0;
-}
